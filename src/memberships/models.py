7e0773a38c69bf118842f32dec2733282eb0df12
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-01 16:22:06 +0600

    Membership model created

--- src/memberships/models.py
+++ src/memberships/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+MEMBERSHIP_CHOICES = (
+    ('Enterprice', 'ent'),
+    ('Professional', 'pro'),
+    ('Free', 'free')
+)
+
+class Membership(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(choices=MEMBERSHIP_CHOICES, max_length=30)
+    price = models.IntegerField(default=15)
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
