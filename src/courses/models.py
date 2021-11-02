035c2c600b0501655b9cdbaa00d4f551fa3518be
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-02 19:23:29 +0600

    two more models added

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from src.memberships.models import Membership
 
+
 # Create your models here.
 
-class Courses(models.Model):
+class Course(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=120)
     description = models.TextField()
@@ -12,4 +13,15 @@ class Courses(models.Model):
     def __str__(self):
         return self.title
 
-    
+
+class Lesson(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    position = models.IntegerField()
+    video_url = models.CharField(max_length=200)
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
