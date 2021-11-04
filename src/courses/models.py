b39b4da8cdeef7e5b1f9b2e8651cca2ae89efbab
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-04 12:49:24 +0600

    problem solved

--- src/courses/models.py
+++ src/courses/models.py
@@ -17,6 +17,10 @@ class Course(models.Model):
     def get_absolute_url(self):
         return reverse('courses:detail', kwargs={'slug': self.slug})
 
+    @property
+    def lessons(self):
+        return self.lesson_set.all().order_by('position')
+
 
 class Lesson(models.Model):
     slug = models.SlugField()
--- src/courses/views.py
+++ src/courses/views.py
@@ -16,11 +16,11 @@ class CourseDetailView(DetailView):
 
 
 class LessonDetailView(View):
-    def get(self, request, course_slug, *args, **kwargs):
+    def get(self, request, course_slug, lesson_slug, *args, **kwargs):
         course_qs = Course.objects.filter(slug=course_slug)
         if course_qs.exists():
             course = course_qs.first()
 
-        lesson_qs = course.lessons??
+        lesson_qs = course.lessons.filter(slug=lesson_slug)
         if lesson_qs.exists():
             lesson = lesson_qs.first()
